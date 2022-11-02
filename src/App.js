3b1690e25675d7baa221884ee83dcac04490455e
Mitali Desai <Mitali.Desai@nfl.com>
2022-11-01 21:51:17 -0400

    Add App Changes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React from 'react';
+//import { StyleSheet, Text, View } from 'react-native';
 
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+  const myStyle= 
+    {
+    backgroundImage: "url(./Images/background_image.png)",
+    height:'50vh',
+    fontSize:'50px',
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    }
+  return <div>  
+  <div style={myStyle}>
+</div>
+<div style={{textAlign:"center"}}> <h1>Please click here to go to Playbook </h1><a href="https://nflplaybook.nfl.net/">
+         <img src=".\Images\playbook.png" alt=""></img></a></div>
+</div>
 }
 
 export default App;
