c48759c8c2575ab35ef07f53b209e71d4bc8a034
Mitali Desai <Mitali.Desai@nfl.com>
2022-11-02 14:43:32 -0400

    updated

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return <div>  
   <div className="backgroundImage">
     <div className="container">
-      <div class="logo"><img src='.\Images\header-shield.png'></img></div>
+      <div class="logo"><img src='.\Images\header-shield.png' alt=""></img></div>
       <a href="https://nflplaybook.nfl.net/"><h1>Please click here to go to </h1><img src=".\Images\playbook.png" alt=""></img></a>
     </div>
 </div>
